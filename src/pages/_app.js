e8c4c004af5c619142d3cdd951c8ed00de8a5f66
theschoolbook <theschoolbook1@gmail.com>
2023-03-10 18:34:07 +0100

    4

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 import '@/styles/globals.css'
 import { AnimatePresence } from 'framer-motion'
 
-export default function App({ Component, pageProps ,router}) {
+export default function App({ Component, pageProps, router }) {
 
-  return <AnimatePresence 
-  mode="wait"
+  return <AnimatePresence
+    mode="wait"
+    initial={false}
+    onExitComplete={() => {
+      console.log("EXIT COMPLETE", router.asPath);
+    }}
   >
     <Component {...pageProps} key={router.pathname} />
   </AnimatePresence>
-  
+
 
 }
